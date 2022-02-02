ef8256fe9f8af4c1555273d2f3777f78429d6947
coperates <dlwoals1195@gmail.com>
2022-02-03 01:43:23 +0900

    add lottie

--- app/src/main/java/com/cookandroid/front_greenmoney/FragmentPage1.java
+++ app/src/main/java/com/cookandroid/front_greenmoney/FragmentPage1.java
@@ -9,17 +9,19 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class FragmentPage1 extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         setHasOptionsMenu(true);
-        return inflater.inflate(R.layout.activity_tree, container, false);
+        ViewGroup Tree_view = (ViewGroup) inflater.inflate(R.layout.activity_tree, container, false);
+        return Tree_view;
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
     }
-
 }
